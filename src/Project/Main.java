0d7bf2418fe55ddb3f1a4c1e33bf86787769d165
VladaWeisberg <e_nagibina01@mail.ru>
2020-10-28 19:53:16 +0300

    Task_6

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -8,84 +8,38 @@ public class Main
     public static void main(String[] args)
     {
         double x = readDouble("Euler number power");
+        double epsilon = readDouble("epsilon");
         int n = readInt("number of term");
 
-        printResults("first", findSumTerms(x, n));
-        printResults("second", findSumBigTerms(x, n));
-        printResults("third", findSumSmallTerms(x, n));
-        printResults("fourth", findFunctionValue(x));
+        printResults("sum", findSumSeries(x, n,0,0));
+        printResults("limited by epsilon", findSumSeries(x, n, 1, epsilon));
+        printResults("limited by epsilon/10", findSumSeries(x, n, 1,epsilon / 10));
+        printResults("right", findRightResult(x));
     }
 
-    private static double findInverseFactorial(int n)
+    private static double findSumSeries(double x, int n, int index, double epsilon)
     {
-        if (n != 0)
-        {
-            int fact = 1;
+        double term = 1;
+        double sum = 1;
+        double sumWithN = 1;
 
-            for (int i = 1; i <= n; i++)
+        for (int i = 1; Math.abs(term) > epsilon; i++)
+        {
+            term *= x / i;
+
+            if (i <= n)
             {
-                fact = fact * i;
+                sum += term;
             }
 
-            return 1.0 / fact;
-        }
-        return 1;
-    }
-
-    private static double findSumElement(double x, int n)
-    {
-        double pow = Math.pow(x, n);
-        double fact = findInverseFactorial(n);
-
-        return pow * fact;
-    }
-    private static double findSumTerms(double x, int n)
-    {
-        double sum = 0;
-
-        for (int i = 0; i < n; i++)
-        {
-            double sumElement = findSumElement(x, i);
-
-            sum = sum + sumElement;
+            sumWithN += term;
         }
 
-        return sum;
+        double[] arraySums= {sum, sumWithN};
+        return arraySums[index];
     }
 
-    private static double findSumBigTerms(double x, int n)
-    {
-        double sum = 0;
-
-        for (int i = 0; i <= n; i++)
-        {
-            double sumElement = findSumElement(x, i);
-
-            if (Math.abs(sumElement) > Math.E)
-            {
-                sum = sum + sumElement;
-            }
-        }
-        return sum;
-    }
-
-    private static double findSumSmallTerms(double x, int n)
-    {
-        double sum = 0;
-
-        for (int i = 0; i <= n; i++)
-        {
-            double sumElement = findSumElement(x, i);
-
-            if (Math.abs(sumElement) > Math.E / 10)
-            {
-                sum = sum + sumElement;
-            }
-        }
-        return sum;
-    }
-
-    private static double findFunctionValue(double x)
+    private static double findRightResult(double x)
     {
         return Math.pow(Math.E, x);
     }
@@ -106,6 +60,6 @@ public class Main
 
     private static void printResults(String name, double result)
     {
-        System.out.printf("The result of %s operation is %1f\n", name, result);
+        System.out.printf("The %s result is %1f\n", name, result);
     }
 }
